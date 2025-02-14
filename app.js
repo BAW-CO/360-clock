7e638a3da5d84bcbdb4bdac4c8292eed34b388b0
BAW-CO <bridgetw.designs@gmail.com>
2025-02-14 14:18:35 -0700

    styling

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
-const millisecondHandle = document.querySelector('.handle__millisecond')
+// const millisecondHandle = document.querySelector('.handle__millisecond')
+// const secondHandle = document.querySelector(".handle__second");
+// const minuteHandle = document.querySelector(".handle__minute");
+// const hourHandle = document.querySelector(".handle__hour");
+
+// requestAnimationFrame(setHandles)
+
+// function setHandles() {
+//   const d = new Date();
+
+//   const millis = d.getMilliseconds();
+//   const hours = d.getHours();
+//   const minutes = d.getMinutes();
+//   const seconds = d.getSeconds();
+
+//   // Increment millisecond hand 360 degrees every 1000ms
+//   const millisAngle = millis * 360 / 1000
+
+//   // Increment hand by 6 degrees every 1000 ms
+//   const secondsScaleFactor = 6 / 1000;
+//   const extraSecondsAngle = millis * secondsScaleFactor;
+//   const secondsAngle = seconds * 6 + extraSecondsAngle
+
+//   // Increment hand 0.1 degrees every second
+//   // 0.1 degree per second = 6 degrees / 60 seconds
+//   const minutesScaleFactor = 6 / 60;
+//   const extraMinutesAngle = seconds * minutesScaleFactor;
+//   const minutesAngle = minutes * 6 + extraMinutesAngle
+  
+//   // Increment hand 0.5 degrees every minute
+//   // 0.5 degree per minute = 30 degrees / 60 minutes
+//   const hoursScaleFactor = 30 / 60;
+//   const extraHoursAngle = minutes * hoursScaleFactor
+//   const hoursAngle = hours * 30 + extraHoursAngle
+
+//   millisecondHandle.style.transform = `translateX(-50%) rotate(${millisAngle}deg)`;
+//   secondHandle.style.transform = `translateX(-50%) rotate(${secondsAngle}deg)`;
+//   hourHandle.style.transform = `translateX(-50%) rotate(${hoursAngle}deg)`;
+//   minuteHandle.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`;
+  
+//   requestAnimationFrame(setHandles)
+// }
+
+
+//::::SOLUTION WITHOUT MILLISECONDHAND::::
+
 const secondHandle = document.querySelector(".handle__second");
 const minuteHandle = document.querySelector(".handle__minute");
 const hourHandle = document.querySelector(".handle__hour");
 
-requestAnimationFrame(setHandles)
+setInterval(() => {
+  setHandles()
+}, 1000)
 
 function setHandles() {
+ 
   const d = new Date();
 
-  const millis = d.getMilliseconds();
   const hours = d.getHours();
   const minutes = d.getMinutes();
   const seconds = d.getSeconds();
 
-  // Increment millisecond hand 360 degrees every 1000ms
-  const millisAngle = millis * 360 / 1000
-
-  // Increment hand by 6 degrees every 1000 ms
-  const secondsScaleFactor = 6 / 1000;
-  const extraSecondsAngle = millis * secondsScaleFactor;
-  const secondsAngle = seconds * 6 + extraSecondsAngle
-
-  // Increment hand 0.1 degrees every second
-  // 0.1 degree per second = 6 degrees / 60 seconds
-  const minutesScaleFactor = 6 / 60;
-  const extraMinutesAngle = seconds * minutesScaleFactor;
+  const extraMinutesAngle = seconds * 0.1
   const minutesAngle = minutes * 6 + extraMinutesAngle
   
-  // Increment hand 0.5 degrees every minute
-  // 0.5 degree per minute = 30 degrees / 60 minutes
-  const hoursScaleFactor = 30 / 60;
-  const extraHoursAngle = minutes * hoursScaleFactor
+  const extraHoursAngle = minutes * 0.5
   const hoursAngle = hours * 30 + extraHoursAngle
 
-  millisecondHandle.style.transform = `translateX(-50%) rotate(${millisAngle}deg)`;
-  secondHandle.style.transform = `translateX(-50%) rotate(${secondsAngle}deg)`;
+  secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`;
   hourHandle.style.transform = `translateX(-50%) rotate(${hoursAngle}deg)`;
   minuteHandle.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`;
-  
-  requestAnimationFrame(setHandles)
 }
